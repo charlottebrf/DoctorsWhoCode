0aef87df6cf584062c3cfc59936726885823aa33
Charlotte Fereday <cbrfereday@gmail.com>
2016-10-30 19:25:58 +0000

    modifies the ruby hash with a minor edit

--- hello.rb
+++ hello.rb
@@ -8,7 +8,7 @@ end
 
 #Get the name of the new activity to add (via post request)
 post ("/create-new-activity") do
-	@activity_name = params[:name]	
+	@activity_name = params[:name]
 	add_new_activity(@activity_name)
 	erb :homepage
 end
@@ -16,9 +16,9 @@ end
 
 #This adds the new activity to a test file for now, to prove the concept
 #Eventually this needs to write new activity to JSON file or database, to the correct file and field
-def add_new_activity(activity_name)	
+def add_new_activity(activity_name)
 	testfile = "./check-my-stuff.txt"
-	target = open(testfile, 'w')	
+	target = open(testfile, 'w')
 	target.write(activity_name)
 	target.close
 end
@@ -32,23 +32,23 @@ end
 	# => @minutes_spent = [data from request.post]
 	# => @activity = [data from request.post]
 	# => log_activity(activity, minutes_spent, date_time) # => log to JSON file or database (should be a separate function, so you can sub out JSON for database. pass in new_activity value)
-	# erb :home 
+	# erb :home
 # end
 #not sure if I need to add in a "def" here like the above code
-#need to check the below code is working, included some sample data 
+#need to check the below code is working, included some sample data
 
 require 'json'
 file = File.read('loggedactivities.json')
 Activities = JSON.parse(file)
 end
-#need to read documentation on whether this is the best place to put the json file 
+#need to read documentation on whether this is the best place to put the json file
 
 Activities =[
 	% : {name: "TV", minutes: "120"}
 	% : {name: "coding", minutes: "45"}]
 
 
-act.add(% name: "", minutes: ""})
+Activities.add(% name: "", minutes: ""})
 
 
 
