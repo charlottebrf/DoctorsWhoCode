446260191daa86c09b0776c57142cfc6b9356b28
Charlotte Fereday <cbrfereday@gmail.com>
2016-10-31 20:29:37 +0000

    adds json write for activities logged

--- hello.rb
+++ hello.rb
@@ -1,6 +1,6 @@
 require 'sinatra'
-
 require 'json'
+require 'date'
 
 
 get '/hello' do
@@ -17,47 +17,31 @@ end
 
 
 #This adds the new activity to a test file for now, to prove the concept
-#Eventually this needs to write new activity to JSON file or database, to the correct file and field
+#Don't forget to initialise the .json file with [] 
 def add_new_activity(activity_name)
-	testfile = "./check-my-stuff.txt"
-	target = open(testfile, 'a')
-	target.write(activity_name)
-	target.close
-
-
-	file = File.open('activitylist.json', "a")
-
-	activities =
-	 {name: activity_name}
-
-	file.write(activities.to_json)
-	file.close
+	json_string = File.read('activitylist.json')
+	activities = JSON.load(json_string)
+	activities << {name: activity_name}
+	File.write('activitylist.json', activities.to_json)
 
 end
 
 
 #Get the name of the activity and minutes spent on it
-# get ("/time-spent-on-activity") do
-	# => how do i indicate a post method for HTML above
-	# => get html form data with equivalent of request.post
-	# => @date_time = [ruby function for date, either date or date time]
-	# => @minutes_spent = [data from request.post]
-	# => @activity = [data from request.post]
-	# => log_activity(activity, minutes_spent, date_time) # => log to JSON file or database (should be a separate function, so you can sub out JSON for database. pass in new_activity value)
-	# erb :home
-# end
-#not sure if I need to add in a "def" here like the above code
-#need to check the below code is working, included some sample data
+post ("/log") do
+	@activity_logged = params[:activity_to_log]
+	@minutes_spent = params[:duration]
+	@date = Date.today
+	log_activity(@activity_logged, @minutes_spent, @date) 
+	erb :homepage
+end
 
-#file = File.read('loggedactivities.json')
+def log_activity(activity_logged, minutes_spent, date)
+	json_string = File.read('loggedactivities.json')
+	activities_logged = JSON.load(json_string)
+	activities_logged << {name: activity_logged, duration: minutes_spent, date: date}
+	File.write('loggedactivities.json', activities_logged.to_json)
 
-#need to read documentation on whether this is the best place to put the json file
+end
 
-
-
-
-
-#log activity data to JSON or database, passing in value from the get_time_spent_on_activity function
-# def log_activity (activity, minutes_spent, date_time)
-	# => write new activity to JSON file or database, to the correct file and field
-# end
+	
