b718b5153db233aaf130bc929c53067cb7f7284f
ajh146 <harris@ainslieharris.com>
2016-11-01 20:11:22 +0000

    added json file parse function and js for text field autocomplete

--- hello.rb
+++ hello.rb
@@ -5,7 +5,24 @@ require 'date'
 
 
 get '/hello' do
+	log_file = 'loggedactivities.json'
+	@log_entries = get_data_from_json_file(log_file)
+
+	#DEBUGGING
+	@log_entries.each do |item|
+  		puts "\nI did #{item["duration"]} minutes of #{item["name"]}\n"
+	end
+	#END OF DEBUGGING
+
 	erb :homepage
+end
+
+
+#Get JSON file data and put it in a Ruby hash
+def get_data_from_json_file(filename)
+	file_to_parse = File.read(filename)
+	data_hash = JSON.parse(file_to_parse)
+	return data_hash
 end
 
 
@@ -48,7 +65,7 @@ end
 	
 # 	#DEBUGGING
 # 	puts "\n\n**These are all the activity names stored**\n\n"	
-# 	puts results
+# 	puts full_list
 # 	#END OF DEBUGGING
 
 # 	return full_list
@@ -83,13 +100,14 @@ end
 #Don't forget to create the table in Heroku's postgres database
 # def log_activity_to_sql(activity_logged, minutes_spent, date)	
 # 	connection = PG.connect(host: "localhost")	#This needs to change when deployed on Heroku...		
-# 	connection.exec("INSERT INTO logged_activities (activity_name, duration, date) VALUES ('#{activity_logged}', '#{minutes_spent}', '#{date}')")	#how to write this to avoid injetion issues?			
+# 	connection.exec("INSERT INTO logged_activities (name, duration, date) VALUES ('#{activity_logged}', '#{minutes_spent}', '#{date}')")	#how to write this to avoid injetion issues?			
 # 	connection.close	#is a commit needed before this?
 # end	
 
 
 
-#Write a function to read the name of each activity from the activitylist JSON file
+
+
 
 #write a function to read the date, name, duration from the loggedactivities JSONfile
 
