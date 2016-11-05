73815b0026ed55fd9eee7303f97e6a22435b6368
ajh146 <harris@ainslieharris.com>
2016-11-05 21:57:08 +0000

    graph works, displays in descending time order

--- hello.rb
+++ hello.rb
@@ -5,42 +5,12 @@ require 'chartkick'
 
 
 get '/hello' do
-	log_file = 'loggedactivities.json'
-	@log_entries = get_data_from_json_file(log_file)
-	@date = Date.today.to_s
-
-
-
-#TEST *** Create array variable to pass to bar chart function
-	today = Date.today.to_s 	#converted to string so it can be compared with hash contents
-	@todays_raw_data = []
-	
-	for entry in @log_entries		
-		if entry["date"] == today		
-			activity = entry["name"]
-			minutes = entry["duration"].to_i			
-			#@todays_raw_data << {activity => minutes}
-
-			@todays_raw_data = {activity => minutes}
-		end		
-	end
- 		
-	#need a handler for if today's data is empty
-
-	#DEBUGGING
-	@log_entries.each do |item|
-  		puts "\nI did #{item["duration"]} minutes of #{item["name"]}\n"
-	end
-
-	puts "\n\n\n ****\nToday is #{today}. There are no activities logged today. See? \n\n This is today's data #{@todays_raw_data}\n\n"
- 	puts "Here's what's in the JSON file:\n #{@log_entries}\n****\n\n\n"		
-	#END OF DEBUGGING
-
+	get_summary_data()
 	erb :homepage
 end
 
 
-#Get data to log (via post request)
+#Get user data for logging (via post request)
 post ("/log") do
 	@activity_logged = params[:activity_to_log]
 	@minutes_spent = params[:duration]
@@ -64,5 +34,25 @@ def get_data_from_json_file(filename)
 	file_to_parse = File.read(filename)
 	data_hash = JSON.parse(file_to_parse)
 	return data_hash
+end
+
+
+#Get log data from today and prepare it for graph/summary in erb file
+def get_summary_data
+	log_file = 'loggedactivities.json'
+	@log_entries = get_data_from_json_file(log_file)
+	@date = Date.today.to_s 	#converted to string so it can be compared with hash contents
+	@todays_data = {}
+
+	for entry in @log_entries		
+		if entry["date"] == @date						
+			@log_entries.each{|k,v| @todays_data[entry["name"]]=entry["duration"].to_i}		#remaps hash to name=>duration pairs
+		end		
+	end
+ 		
+ 	@graph_data = @todays_data.sort_by { |k,v| -v}
+ 	
+	#need a handler for if today's data is empty
+
 end
 
