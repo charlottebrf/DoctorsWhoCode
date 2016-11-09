0807250059853e1db5ef29aea3f31a724cb8bac6
ajh146 <harris@ainslieharris.com>
2016-11-09 02:46:44 +0000

    added target setting functionality

--- homepage.rb
+++ homepage.rb
@@ -75,6 +75,22 @@ get '/month' do
   erb :month
 end
 
+get '/targets' do
+  halt erb(:notauth) unless admin?
+  @autocomplete_suggestions = get_autocomplete_suggestions()
+  get_target_data()
+  erb :targets
+end
+
+post ('/settarget') do
+  @target_activity = params[:target_activity]
+  puts "\n\n\n #{@target_activity}"
+  @target_minutes = params[:target_minutes]
+  @period = params[:period]
+  log_targets_to_json(@target_activity, @target_minutes, @period) 
+  redirect "/homepage"
+end
+
 
 get '/logout' do
   session[:admin] = nil
@@ -91,6 +107,15 @@ post ("/log") do
   @date = Date.today
   log_activity_to_json(@user, @activity_logged, @minutes_spent, @date, @type) 
   redirect "/homepage"
+end
+
+
+#Don't forget to initialise the .json file with [] (this means we need to push such a file to heroku for demo day)
+def log_targets_to_json(target_activity, target_minutes, period)
+  json_string = File.read('targets.json')
+  targets_set = JSON.load(json_string)
+  targets_set << {user: $user_id, target_activity: target_activity, target_minutes: target_minutes, period: period}
+  File.write('targets.json', targets_set.to_json)
 end
 
 
@@ -134,66 +159,29 @@ def get_today_summary_data
   @todays_text_data = {}
   all_activities_entered_today =[]
   interim_activity_list = []  #intermediate step so we can up up all durations for a single activity
-
   for entry in @log_entries   
     if (entry["date"] == today) && (entry["user"] == $user_id)
       all_activities_entered_today << {"name" => entry["name"], "duration" => entry["duration"].to_i} 
       interim_activity_list << {"name" => entry["name"], "duration" => 0}     
     end   
   end
-
   day_summary_of_unique_activities = interim_activity_list.uniq { |row| row["name"] } #creates list of unique activities based on name
-
   for entry in day_summary_of_unique_activities
-
     for item in all_activities_entered_today
-
-      if entry["name"] == item["name"]
-        
-        #DEBUGGING#
-        #This should find a match for every activity plus each duplicate
-        puts "\n\n***IT'S A MATCH!***"
-        puts "The activity \"#{entry["name"]}\" in the list of distinct activities matched with \"#{item["name"]}\" in the log file for today."
-        puts "Add #{item["duration"]} minutes to the running total of #{entry["duration"]} minutes."
-        #END OF DEBUGGING#
-
-        #Adds to duration for every value/duplicate value found
-        entry["duration"] += item["duration"]
-        
-        #DEBUGGING#
-        puts "Today's new total for \"#{entry["name"]}\" is  #{entry["duration"]} minutes"
-        #END OF DEBUGGING#
-
+      if entry["name"] == item["name"]            
+        entry["duration"] += item["duration"]         #Adds to duration for every value/duplicate value found
       end
-
     end
-
   end
-
   #remap hashes to name=>duration pairs
   for entry in day_summary_of_unique_activities
     day_summary_of_unique_activities.each{|k,v| @todays_graph_data[entry["name"].upcase]=entry["duration"].to_i}    
     day_summary_of_unique_activities.each{|k,v| @todays_text_data[entry["name"]]=entry["duration"].to_i}
-  end
-    
+  end    
   @graph_data = @todays_graph_data.sort_by { |k,v| -v} 
   @text_data = @todays_text_data.sort_by { |k,v| -v} 
   @most_time_spent = @text_data[0]
   @least_time_spent = @text_data[-1]  
-
-  #debugging#
-  puts "\n\n\n***** JSON, hash, and array debugging *****"
-  puts "\n ALL JSON LOG ENTRIES:\n#{@log_entries}"
-  puts "\n Todays Raw Data:\n#{all_activities_entered_today}"
-  puts "\n interim_activity_list:\n#{interim_activity_list}"
-  puts "\n Today time spent on activities today:\n#{day_summary_of_unique_activities}"
-  puts "\n TODAY'S LOGGED GRAPH DATA:\n#{@todays_graph_data}"
-  puts "\n TODAY'S LOGGED TEXT DATA:\n#{@todays_text_data}"
-  puts "\n SORTED GRAPH DATA:\n#{@graph_data}"
-  puts "\n SORTED TEXT DATA:\n#{@text_data}"
-  puts "\n\n\n"
-  #end of debugging#
-
 end
 
 
@@ -207,16 +195,13 @@ def get_week_summary_data
   @week_text_data = {}
   all_activities_entered_week =[]
   interim_activity_list = []  
-
   for entry in @log_entries   
     if ((entry["date"] <= today) && (entry["date"] >= a_week_ago)) && (entry["user"] == $user_id)
       all_activities_entered_week << {"name" => entry["name"], "duration" => entry["duration"].to_i}  
       interim_activity_list << {"name" => entry["name"], "duration" => 0}     
     end   
   end
-
   week_summary_of_unique_activities = interim_activity_list.uniq { |row| row["name"] }
-
   for entry in week_summary_of_unique_activities
     for item in all_activities_entered_week
       if entry["name"] == item["name"]                
@@ -224,12 +209,10 @@ def get_week_summary_data
       end
     end
   end
-
   for entry in week_summary_of_unique_activities
     week_summary_of_unique_activities.each{|k,v| @week_graph_data[entry["name"].upcase]=entry["duration"].to_i}   
     week_summary_of_unique_activities.each{|k,v| @week_text_data[entry["name"]]=entry["duration"].to_i}
   end
-    
   @graph_data_week = @week_graph_data.sort_by { |k,v| -v} 
   @text_data_week = @week_text_data.sort_by { |k,v| -v} 
   @most_time_spent_week = @text_data_week[0]
@@ -247,16 +230,13 @@ def get_month_summary_data
   @month_text_data = {}
   all_activities_entered_month =[]
   interim_activity_list = []  
-
   for entry in @log_entries   
     if ((entry["date"] <= today) && (entry["date"] >= a_month_ago)) && (entry["user"] == $user_id)
       all_activities_entered_month << {"name" => entry["name"], "duration" => entry["duration"].to_i} 
       interim_activity_list << {"name" => entry["name"], "duration" => 0}     
     end   
   end
-
   month_summary_of_unique_activities = interim_activity_list.uniq { |row| row["name"] }
-
   for entry in month_summary_of_unique_activities
     for item in all_activities_entered_month
       if entry["name"] == item["name"]                
@@ -264,14 +244,44 @@ def get_month_summary_data
       end
     end
   end
-
   for entry in month_summary_of_unique_activities
     month_summary_of_unique_activities.each{|k,v| @month_graph_data[entry["name"].upcase]=entry["duration"].to_i}   
     month_summary_of_unique_activities.each{|k,v| @month_text_data[entry["name"]]=entry["duration"].to_i}
   end
-    
   @graph_data_month = @month_graph_data.sort_by { |k,v| -v} 
   @text_data_month = @month_text_data.sort_by { |k,v| -v} 
   @most_time_spent_month = @text_data_month[0]
   @least_time_spent_month = @text_data_month[-1]  
+end
+
+
+def get_target_data
+  target_file = 'targets.json'
+  @target_entries = get_data_from_json_file(target_file)
+
+  @day_targets = []
+  @week_targets = []  
+  @month_targets = []
+
+  for entry in @target_entries   
+    if (entry["user"] == $user_id) && (entry["period"] == "day")
+      @day_targets << {"user" => entry["user"], "target_activity" => entry["target_activity"], "target_minutes" => entry["target_minutes"], "period" => entry["period"]}
+    end   
+    if (entry["user"] == $user_id) && (entry["period"] == "week")
+      @week_targets << {"user" => entry["user"], "target_activity" => entry["target_activity"], "target_minutes" => entry["target_minutes"], "period" => entry["period"]}
+    end  
+    if (entry["user"] == $user_id) && (entry["period"] == "month")
+      @month_targets << {"user" => entry["user"], "target_activity" => entry["target_activity"], "target_minutes" => entry["target_minutes"], "period" => entry["period"]}
+    end  
+  end
+
+  # DEBUGGING
+  puts "\n\n\n***********"
+  puts @target_entries
+  puts @day_targets
+  puts @week_targets
+  puts @month_targets
+  puts "\n\n\n***********"
+  # DEBUGGING
+
 end
