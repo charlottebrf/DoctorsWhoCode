0f8978cdc9db7d8fbde248b012c70cceb6c404c2
Charlotte Fereday <cbrfereday@gmail.com>
2016-11-17 07:52:59 +0000

    adds Consumer Key & Secret to Twitter authentication

--- homepage.rb
+++ homepage.rb
@@ -6,13 +6,14 @@ require 'oauth'
 
 
 use OmniAuth::Builder do
-  provider :twitter, '8aLsCR1VgiOHWQ5GmiGhPYrqG', 'cYcZsp8sCh9bR0MDbhyPr51uWajq0GGopOrnEOOiBL1s2eq7nT',
+  provider :twitter, ENV['CONSUMER_KEY'], ENV ['CONSUMER_SECRET']
   {:authorize_params => {
     :force_login => 'true'
     }
   }
 end
-
+#Line 9 has been updated with CONSUMER_KEY & CONSUMER_SECRET which were created
+#via Twitter Twitter Dev
 
 configure do
   enable :sessions
@@ -41,10 +42,11 @@ end
 
 
 get '/auth/twitter/callback' do
-  session[:admin] = true     
+  session[:admin] = true
   #If you halt and restart Ruby, these values get lost when pages are refreshed. Could be an issue if user never logs out and servers go to sleep
   $user_name = env['omniauth.auth']['info']['name']
   $user_id = env['omniauth.auth']['uid']
+  puts $user_id
   redirect to("/homepage")
 end
 
@@ -99,19 +101,19 @@ post ('/settarget') do
   puts "\n\n\n #{@target_activity}"
   @target_minutes = params[:target_minutes]
   @period = params[:period]
-  log_targets_to_json(@target_activity, @target_minutes, @period) 
+  log_targets_to_json(@target_activity, @target_minutes, @period)
   redirect "/targets"
 end
 
 
 post ('/deletetarget') do
   data_hash = get_data_from_json_file("targets.json")
-  data_for_target_to_delete = params[:delete_target].split(%r{,\s*})  
+  data_for_target_to_delete = params[:delete_target].split(%r{,\s*})
   for entry in data_hash
     if (entry["target_minutes"] == data_for_target_to_delete[0]) && (entry["target_activity"] == data_for_target_to_delete[1]) && (entry["period"] == data_for_target_to_delete[2])
        @updated_target_list = data_hash.delete_if { |h| (h["target_minutes"] == data_for_target_to_delete[0] && h["target_activity"] == data_for_target_to_delete[1] && h["period"] == data_for_target_to_delete[2]) }
     end
-  end  
+  end
   write_updated_target_list_to_json(@updated_target_list)
   redirect "/targets"
 end
@@ -138,7 +140,7 @@ post ("/log") do
   if @activity_logged =~ /\s*/
     @activity_logged = @activity_logged.squeeze(" ")
   end
-  log_activity_to_json(@user, @activity_logged, @minutes_spent, @date, @type) 
+  log_activity_to_json(@user, @activity_logged, @minutes_spent, @date, @type)
   redirect "/homepage"
 end
 
@@ -181,8 +183,8 @@ def get_autocomplete_suggestions (log_entries)
       my_activities_only << {"user" => entry["user"], "name" => entry["name"], "type" => entry["type"], "duration" => entry["duration"], "date" => entry["date"]}
     end
   end
-  autocomplete_suggestions = my_activities_only.uniq { |row| row["name"] }   
-  return autocomplete_suggestions 
+  autocomplete_suggestions = my_activities_only.uniq { |row| row["name"] }
+  return autocomplete_suggestions
 end
 
 def load_data #load data once on homepage
@@ -198,21 +200,21 @@ def get_data(activity_list, interim_activity_list, key_name)
   summary_of_unique_activities = interim_activity_list.uniq { |row| row[key_name] } #creates list of unique activities based on name
   for entry in summary_of_unique_activities
     for item in activity_list
-      if entry[key_name] == item[key_name]            
+      if entry[key_name] == item[key_name]
         entry["duration"] += item["duration"]         #Adds to duration for every value/duplicate value found
       end
     end
   end
   #remap hashes to name=>duration pairs
   for entry in summary_of_unique_activities
-    summary_of_unique_activities.each{|k,v| graph_data[entry[key_name].upcase]=entry["duration"].to_i}    
+    summary_of_unique_activities.each{|k,v| graph_data[entry[key_name].upcase]=entry["duration"].to_i}
     summary_of_unique_activities.each{|k,v| text_data[entry[key_name]]=entry["duration"].to_i}
   end
 
 #reduces replicated code filtering data from json
   summary_data = Hash.new
-  summary_data["graph_data"] = graph_data.sort_by { |k,v| -v} 
-  summary_data["text_data"] = text_data.sort_by { |k,v| -v} 
+  summary_data["graph_data"] = graph_data.sort_by { |k,v| -v}
+  summary_data["text_data"] = text_data.sort_by { |k,v| -v}
   summary_data["most_time_spent"] = summary_data["text_data"][0]
   summary_data["least_time_spent"] = summary_data["text_data"][-1]
 
@@ -223,15 +225,15 @@ end
 def get_today_summary_data (log_entries)
 
   # Refactor log file parsing into external method and pass in as local parameter, not instance variable (no '@')
-  
+
   today = Date.today.to_s   #convert to string so it can be compared with hash contents
   all_activities_entered_today =[]
   interim_activity_list = []  #intermediate step so we can up up all durations for a single activity
-  for entry in log_entries   
+  for entry in log_entries
     if (entry["date"] == today) && (entry["user"] == $user_id)
-      all_activities_entered_today << {"name" => entry["name"].upcase, "duration" => entry["duration"].to_i} 
-      interim_activity_list << {"name" => entry["name"].upcase, "duration" => 0}     
-    end   
+      all_activities_entered_today << {"name" => entry["name"].upcase, "duration" => entry["duration"].to_i}
+      interim_activity_list << {"name" => entry["name"].upcase, "duration" => 0}
+    end
   end
 
   return get_data(all_activities_entered_today, interim_activity_list, "name") # Retrieve from common method
@@ -243,11 +245,11 @@ def get_today_work_data (log_entries)
   todays_text_data = {}
   all_activities_entered_today =[]
   interim_activity_list = []  #intermediate step so we can up up all durations for a single activity
-  for entry in log_entries   
+  for entry in log_entries
     if (entry["date"] == today) && (entry["user"] == $user_id)
-      all_activities_entered_today << {"type" => entry["type"], "duration" => entry["duration"].to_i} 
-      interim_activity_list << {"type" => entry["type"], "duration" => 0}     
-    end   
+      all_activities_entered_today << {"type" => entry["type"], "duration" => entry["duration"].to_i}
+      interim_activity_list << {"type" => entry["type"], "duration" => 0}
+    end
   end
 
   return get_data(all_activities_entered_today, interim_activity_list, "type") # Retrieve from common method
@@ -257,54 +259,54 @@ end
 def get_week_summary_data (log_entries)
   now = Date.today
   today = now.to_s
-  a_week_ago = (now-7).to_s   
+  a_week_ago = (now-7).to_s
   week_graph_data = {}
   week_text_data = {}
   all_activities_entered_week =[]
-  interim_activity_list = []  
-  for entry in log_entries   
+  interim_activity_list = []
+  for entry in log_entries
     if ((entry["date"] <= today) && (entry["date"] >= a_week_ago)) && (entry["user"] == $user_id)
-      all_activities_entered_week << {"name" => entry["name"], "duration" => entry["duration"].to_i}  
-      interim_activity_list << {"name" => entry["name"], "duration" => 0}     
-    end   
+      all_activities_entered_week << {"name" => entry["name"], "duration" => entry["duration"].to_i}
+      interim_activity_list << {"name" => entry["name"], "duration" => 0}
+    end
   end
 
-  return get_data(all_activities_entered_week, interim_activity_list, "name") # Retrieve from common method 
+  return get_data(all_activities_entered_week, interim_activity_list, "name") # Retrieve from common method
 end
 
 
 def get_week_work_data (log_entries)
   now = Date.today
   today = now.to_s
-  a_week_ago = (now-7).to_s   
+  a_week_ago = (now-7).to_s
   week_graph_data = {}
   week_text_data = {}
   all_activities_entered_week =[]
-  interim_activity_list = []  
-  for entry in log_entries   
+  interim_activity_list = []
+  for entry in log_entries
     if ((entry["date"] <= today) && (entry["date"] >= a_week_ago)) && (entry["user"] == $user_id)
-      all_activities_entered_week << {"type" => entry["type"], "duration" => entry["duration"].to_i}  
-      interim_activity_list << {"type" => entry["type"], "duration" => 0}     
-    end   
+      all_activities_entered_week << {"type" => entry["type"], "duration" => entry["duration"].to_i}
+      interim_activity_list << {"type" => entry["type"], "duration" => 0}
+    end
   end
 
-  return get_data(all_activities_entered_week, interim_activity_list, "type") # Retrieve from common method 
+  return get_data(all_activities_entered_week, interim_activity_list, "type") # Retrieve from common method
 end
 
 
 def get_month_summary_data (log_entries)
   now = Date.today
   today = now.to_s
-  a_month_ago = (now-30).to_s   
+  a_month_ago = (now-30).to_s
   month_graph_data = {}
   month_text_data = {}
   all_activities_entered_month =[]
-  interim_activity_list = []  
-  for entry in log_entries   
+  interim_activity_list = []
+  for entry in log_entries
     if ((entry["date"] <= today) && (entry["date"] >= a_month_ago)) && (entry["user"] == $user_id)
-      all_activities_entered_month << {"name" => entry["name"], "duration" => entry["duration"].to_i} 
-      interim_activity_list << {"name" => entry["name"], "duration" => 0}     
-    end   
+      all_activities_entered_month << {"name" => entry["name"], "duration" => entry["duration"].to_i}
+      interim_activity_list << {"name" => entry["name"], "duration" => 0}
+    end
   end
 
   return get_data(all_activities_entered_month, interim_activity_list, "name") # Retrieve from common method
@@ -314,16 +316,16 @@ end
 def get_month_work_data (log_entries)
   now = Date.today
   today = now.to_s
-  a_month_ago = (now-30).to_s   
+  a_month_ago = (now-30).to_s
   month_graph_data = {}
   month_text_data = {}
   all_activities_entered_month =[]
-  interim_activity_list = []  
-  for entry in log_entries   
+  interim_activity_list = []
+  for entry in log_entries
     if ((entry["date"] <= today) && (entry["date"] >= a_month_ago)) && (entry["user"] == $user_id)
-      all_activities_entered_month << {"type" => entry["type"], "duration" => entry["duration"].to_i} 
-      interim_activity_list << {"type" => entry["type"], "duration" => 0}     
-    end   
+      all_activities_entered_month << {"type" => entry["type"], "duration" => entry["duration"].to_i}
+      interim_activity_list << {"type" => entry["type"], "duration" => 0}
+    end
   end
 
   return get_data(all_activities_entered_month, interim_activity_list, "type") # Retrieve from common method
@@ -334,19 +336,19 @@ def get_target_data
   @target_entries = get_data_from_json_file(target_file)
 
   @day_targets = []
-  @week_targets = []  
+  @week_targets = []
   @month_targets = []
 
-  for entry in @target_entries   
+  for entry in @target_entries
     if (entry["user"] == $user_id) && (entry["period"] == "day")
       @day_targets << {"user" => entry["user"], "target_activity" => entry["target_activity"], "target_minutes" => entry["target_minutes"], "period" => entry["period"]}
-    end   
+    end
     if (entry["user"] == $user_id) && (entry["period"] == "week")
       @week_targets << {"user" => entry["user"], "target_activity" => entry["target_activity"], "target_minutes" => entry["target_minutes"], "period" => entry["period"]}
-    end  
+    end
     if (entry["user"] == $user_id) && (entry["period"] == "month")
       @month_targets << {"user" => entry["user"], "target_activity" => entry["target_activity"], "target_minutes" => entry["target_minutes"], "period" => entry["period"]}
-    end  
+    end
   end
 
 end
